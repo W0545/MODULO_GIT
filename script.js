64019045d4f039a1e398a1c0cd3730c54d2c2a0a
Wellerson Ramos <Wellersonr378@gmail.com>
2024-05-29 16:09:53 -0300

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
-document.getElementById('myForm').addEventListener('submit', function(event) {
+document.getElementById('contactForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    
-    const campoA = parseFloat(document.getElementById('campoA').value);
-    const campoB = parseFloat(document.getElementById('campoB').value);
-    const messageDiv = document.getElementById('message');
-    
-    if (campoB > campoA) {
-        messageDiv.textContent = 'Formulário válido! O número B é maior que o número A.';
-        messageDiv.className = 'message positive';
-    } else {
-        messageDiv.textContent = 'Formulário inválido. O número B deve ser maior que o número A.';
-        messageDiv.className = 'message negative';
+
+    var nome = document.getElementById('nome').value;
+    var telefone = document.getElementById('telefone').value;
+
+    if (!validatePhone(telefone)) {
+        alert('Por favor, insira um número de telefone válido.');
+        return;
     }
+
+    var table = document.getElementById('contactsTable').getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow();
+
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+
+    cell1.textContent = nome;
+    cell2.textContent = telefone;
+
+    document.getElementById('contactForm').reset();
 });
+
+function validatePhone(phone) {
+    var phoneRegex = /^\(?\d{2}\)?[\s-]?[\s9]?\d{4}-?\d{4}$/;
+    return phoneRegex.test(phone);
+}
